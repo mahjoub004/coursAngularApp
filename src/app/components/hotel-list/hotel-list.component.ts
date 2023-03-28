e8d445d7605ef5a2e49f358c28f2d20d54d60b44
jebalimahjoub <mahjoub_jebali@yahoo.fr>
2023-03-28 16:45:22 +0200

    course data binding one way {{..}} [..]  and directive *ngIf , *ngFor

--- src/app/components/hotel-list/hotel-list.component.ts
+++ src/app/components/hotel-list/hotel-list.component.ts
@@ -7,9 +7,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HotelListComponent implements OnInit {
 
+  hotels: any[] = [
+    {
+          "hotelId": 1,
+          "hotelName": "Buea sweet life",
+          "description": "Belle vue au bord de la mer",
+          "price": 230.5,
+          "imageUrl": "assets/img/hotel1.jpg",
+          "rating": 3.5
+    },
+    {
+          "hotelId": 2,
+          "hotelName": "Marakech",
+          "description": "Profitez de la vue sur les montagnes",
+          "price": 145.5,
+          "imageUrl": "assets/img/hotel2.jpg",
+          "rating": 5
+    },
+    {
+          "hotelId": 3,
+          "hotelName": "Abudja new look palace",
+          "description": "Séjour complet avec service de voitures",
+          "price": 120.12,
+          "imageUrl": "assets/img/hotel3.jpg",
+          "rating": 4
+    },
+    {
+          "hotelId": 4,
+          "hotelName": "Cape town city",
+          "description": "Magnifique cadre pour votre séjour",
+          "price": 135.12,
+          "imageUrl": "assets/img/hotel4.jpg",
+          "rating": 2.5
+    }
+    ,
+    {
+          "hotelId": 4,
+          "hotelName": "Cape town city",
+          "description": "Magnifique cadre pour votre séjour",
+          "price": 135.12,
+          "imageUrl": "assets/img/hotel4.jpg",
+          "rating": 2.5
+    }
+  ]
+  public showBadge: boolean =false;
   constructor() { }
 
   ngOnInit(): void {
+
   }
+   /**
+     * toggleIsNewBadge
+ : void    */
+ public toggleIsNewBadge(): void {
+  this.showBadge = !this.showBadge
+}
 
 }
