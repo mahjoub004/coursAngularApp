d071220a1e18430ef667202236793eec35f504a4
jebalimahjoub <mahjoub_jebali@yahoo.fr>
2023-03-29 15:40:19 +0200

    Comprendre les services et l'injection des dépendances

--- src/app/components/hotel-list/hotel-list.component.ts
+++ src/app/components/hotel-list/hotel-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ihotel } from 'src/app/interfaces/ihotel';
+import { HotelListServiceService } from 'src/app/services/hotel-list-service/hotel-list-service.service';
 
 @Component({
   selector: 'app-hotel-list',
@@ -7,50 +8,10 @@ import { Ihotel } from 'src/app/interfaces/ihotel';
   styleUrls: ['./hotel-list.component.css']
 })
 export class HotelListComponent implements OnInit {
+    constructor(private hotelsService: HotelListServiceService) { }
 
-  hotels: Ihotel[] = [
-    {
-          "hotelId": 1,
-          "hotelName": "Buea sweet life",
-          "description": "Belle vue au bord de la mer",
-          "price": 230.5,
-          "imageUrl": "assets/img/hotel1.jpg",
-          "rating": 3.5
-    },
-    {
-          "hotelId": 2,
-          "hotelName": "Marakech",
-          "description": "Profitez de la vue sur les montagnes",
-          "price": 145.5,
-          "imageUrl": "assets/img/hotel2.jpg",
-          "rating": 5
-    },
-    {
-          "hotelId": 3,
-          "hotelName": "Abudja new look palace",
-          "description": "Séjour complet avec service de voitures",
-          "price": 120.12,
-          "imageUrl": "assets/img/hotel3.jpg",
-          "rating": 4
-    },
-    {
-          "hotelId": 4,
-          "hotelName": "Cape town city",
-          "description": "Magnifique cadre pour votre séjour",
-          "price": 135.12,
-          "imageUrl": "assets/img/hotel4.jpg",
-          "rating": 2.5
-    }
-    ,
-    {
-          "hotelId": 4,
-          "hotelName": "Cape town city",
-          "description": "Magnifique cadre pour votre séjour",
-          "price": 135.12,
-          "imageUrl": "assets/img/hotel4.jpg",
-          "rating": 2.5
-    }
-  ]
+  hotels: Ihotel[] = [];
+
   public showBadge: boolean =false;
 
   private _hotelFilter = 'mot';
@@ -59,9 +20,10 @@ export class HotelListComponent implements OnInit {
 
   public receivedRating: string| undefined;
 
-  constructor() { }
+
 
   ngOnInit(): void {
+    this.hotels = this.hotelsService.gestHotels();
     this.filteredHotels = this.hotels
     this.hotelFilter = ""
   }
